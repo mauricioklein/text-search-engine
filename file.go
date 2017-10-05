4726231fea0fbdd021c94666ea7796cf52591295
Mauricio Klein <mauricio.klein@fromatob.com>
2017-10-05 14:01:04 +0200

    Add File structure to system

---
+++ file.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"strings"
+)
+
+// File is the in-memory representation of a OS file
+type File struct {
+	os.FileInfo
+	Path    string
+	Content string
+}
+
+// NewFile returns a new instance of File
+func NewFile(path string, info os.FileInfo, content string) File {
+	return File{
+		FileInfo: info,
+		Path:     path,
+		Content:  content,
+	}
+}
+
+// Words split the file content in every whitespace and
+// line break and return a slice of words
+func (file File) Words() []string {
+	oneLineContent := strings.Replace(file.Content, "\n", " ", -1)
+	return strings.Split(oneLineContent, " ")
+}
