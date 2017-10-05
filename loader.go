0df2e4c2b049fb913556aee51ba4ae33076df168
Mauricio Klein <mauricio.klein@fromatob.com>
2017-10-05 11:05:06 +0200

    Add support to DiskReader

---
+++ loader.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"io/ioutil"
+)
+
+// Reader defines an interface that implements the
+// methods to read files from some resource and translate
+// it to File instances
+type Reader interface {
+	Read(path string) ([]File, error)
+}
+
+// DiskReader defines a implementation of the
+// Reader interface to read files from Disk
+type DiskReader struct{}
+
+// Read reads the files from disk and translate it
+// to the internal File structure
+func (dr DiskReader) Read(path string) ([]File, error) {
+	var files []File
+
+	memFiles, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, memFile := range memFiles {
+		if memFile.IsDir() {
+			continue
+		}
+
+		content, err := ioutil.ReadFile(path + memFile.Name())
+		if err != nil {
+			return nil, err
+		}
+
+		files = append(files, File{
+			FileInfo: memFile,
+			Path:     path,
+			Content:  string(content),
+		})
+	}
+
+	return files, nil
+}
---
+++ loader_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadFromDiskSuccess(t *testing.T) {
+	files, err := DiskReader{}.Read("./test-utils/files/")
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.Len(t, files, 2)
+
+	// file1.txt
+	file := files[0]
+	assert.Implements(t, (*os.FileInfo)(nil), file.FileInfo)
+	assert.Equal(t, "./test-utils/files/", file.Path)
+	assert.Equal(t, fileContent("./test-utils/files/file1.txt"), file.Content)
+
+	// file1.txt
+	file = files[1]
+	assert.Implements(t, (*os.FileInfo)(nil), file.FileInfo)
+	assert.Equal(t, "./test-utils/files/", file.Path)
+	assert.Equal(t, fileContent("./test-utils/files/file2.txt"), file.Content)
+}
+
+func TestLoadFromDiskFailure(t *testing.T) {
+	_, err := DiskReader{}.Read("./foo/bar")
+	if err == nil {
+		t.Errorf("expected error, got nil")
+	}
+}
+
+func fileContent(path string) string {
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return ""
+	}
+
+	return string(bytes)
+}
