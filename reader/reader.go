da4fd98f1535e3c5a4c95ff7f7f0f931d83bc984
Mauricio Klein <mauricio.klein@fromatob.com>
2017-10-07 00:30:50 +0200

    Cleanup reader structure

--- reader/reader.go
+++ reader/reader.go
@@ -1,5 +1,14 @@
 package reader
 
+import "os"
+
+// File is the in-memory representation of a OS file
+type File struct {
+	os.FileInfo
+	Path    string
+	Content string
+}
+
 // Reader defines an interface that implements the
 // methods to read files from some resource and translate
 // it to File instances
